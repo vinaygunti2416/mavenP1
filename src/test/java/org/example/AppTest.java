803d07949d45bb7177665f0a124011ae8e27aa38
vinaygunti2416 <123142100+vinaygunti2416@users.noreply.github.com>
2023-04-21 10:03:24 +0530

    Update AppTest.java

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,23 +1,59 @@
 package org.example;
-import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
+
+
+
 public class AppTest
 {
-    @Test
-    public void testcase1_login()
+    WebDriver driver;
+    @BeforeClass
+    @Parameters({"browser","url"})
+    void setupdrive(String browser,String link)
     {
-        App a=new App();
-        Assert.assertEquals(true,a.login_test("vinay","vinay123"));
+        if(browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver=new ChromeDriver();
+        }
+        else if(browser.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            driver=new FirefoxDriver();
+        }
+        driver.get(link);
+        driver.manage().window().maximize();
     }
 
+
+
     @Test
-    public void testcase2_login(){
-        App a1= new App();
-        Assert.assertEquals(false, a1.login_test("user","pass"));
+    void dropdownmethod()throws InterruptedException{
+
+        Thread.sleep(8000);
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div/select[1]/option[3]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div/select[2]/option[3]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div/select[3]/option[3]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/div[3]/div/div/label[2]/input")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/div[4]/div/div/form/input[3]")).click();
+        Thread.sleep(3000);
+
+    }
+
+
+    @AfterClass
+    void driveclose(){
+        driver.close();
     }
 
 }
